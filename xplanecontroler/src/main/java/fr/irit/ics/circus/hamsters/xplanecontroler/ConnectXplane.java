8563b1831ccfa6f964fa25386c93e8ee523b6864
Eric Barboni <skygo@apache.org>
2018-06-27 18:05:54 +0200

    send RREF

--- xplanecontroler/src/main/java/fr/irit/ics/circus/hamsters/xplanecontroler/ConnectXplane.java
+++ xplanecontroler/src/main/java/fr/irit/ics/circus/hamsters/xplanecontroler/ConnectXplane.java
@@ -10,11 +10,17 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.ServerSocket;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -25,12 +31,43 @@ public class ConnectXplane {
     private final int xplanePort;
     private final InetAddress xplaneAddr;
     private final DatagramSocket socket;
+    private final ServerSocket listenersocket;
 
-    public ConnectXplane() throws SocketException, UnknownHostException {
-        this.socket = new DatagramSocket(0);
+    public ConnectXplane() throws SocketException, UnknownHostException, IOException {
+        this.socket = new MulticastSocket(0);
         this.xplaneAddr = InetAddress.getByAddress(new byte[]{(byte) 141, (byte) 115, (byte) 66, (byte) 26});
         this.xplanePort = 49000;
-        this.socket.setSoTimeout(30);
+        this.socket.setSoTimeout(1000);  // this.socket.set
+        this.socket.setReuseAddress(true);
+        this.listenersocket = new ServerSocket(0/*, xplaneAddr*/);
+     //   listenersocket.accept();
+        //    this.socket = new DatagramSocket(0);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    byte[] buffer = new byte[65536];
+                    DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                    try {
+                        System.err.println("dsd");
+                        socket.receive(packet);
+                        // System.err.println(packet.getLength());
+                        Arrays.copyOf(buffer, packet.getLength());
+                        for (int i = 0; i < packet.getLength(); i++) {
+                            System.err.print(Character.toString((char) buffer[i]));
+                        }
+                        System.err.println("");
+                        //  System.err.println(Character.toString((char) buffer[0]) + " " + Character.toString((char) buffer[1]) + " " +Character.toString((char) buffer[2]));
+                    } catch (SocketTimeoutException ex) {
+                        System.err.println("timeout" + ex.getMessage());
+                    } catch (IOException ex) {
+                        Logger.getLogger(ConnectXplane.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                }
+            }
+        })
+                .start();
     }
 
     private void sendUDP(byte[] buffer) throws IOException {
@@ -38,6 +75,18 @@ public class ConnectXplane {
         socket.send(packet);
     }
 
+    /*
+    private byte[] readUDP() throws IOException {
+        byte[] buffer = new byte[65536];
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+        try {
+            socket.receive(packet);
+            return Arrays.copyOf(buffer, packet.getLength());
+        } catch (SocketTimeoutException ex) {
+            return new byte[0];
+        }
+    }
+     */
     public void sendDREF(String drefs, float values) throws IOException {
         //Preconditions
         /* if (drefs == null || drefs.length == 0) {
@@ -88,22 +137,49 @@ public class ConnectXplane {
             os.write(0x20);
         }
 
-        System.err.println(os.size());
+        // System.err.println(os.size());
         sendUDP(os.toByteArray());
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ConnectXplane.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
+    static int ID;
 
-    /* public void sendDREF(String drefs, int values) throws IOException {
+    public int getDREFs(String drefs, int freqHZ) throws IOException {
         //Preconditions
-       
-
+        /* if (drefs == null || drefs.length == 0) {
+            throw new IllegalArgumentException(("drefs must be non-empty."));
+        }*/
+ /*if (values == null || values.length != drefs.length) {
+            throw new IllegalArgumentException("values must be of the same size as drefs.");
+        }*/
+        int myiD = ID++;
         ByteArrayOutputStream os = new ByteArrayOutputStream();
-        os.write("DREF".getBytes(StandardCharsets.UTF_8));
+        os.write("RREF".getBytes(StandardCharsets.UTF_8));
         os.write(0x00); //Placeholder for message length
-
+        // freq
+        os.write(0x01);
+        os.write(0x00);
+        os.write(0x00);
+        os.write(0x00);
+//id
+        os.write(0x05);
+        os.write(0x00);
+        os.write(0x00);
+        os.write(0x00);
+        //   os.write(myiD);
         String dref = drefs;
-        float value = values;
 
-       
+
+        /* if (dref == null) {
+            throw new IllegalArgumentException("dref must be a valid string.");
+        }
+        if (value == null || value.length == 0) {
+            throw new IllegalArgumentException("value must be non-null and should contain at least one value.");
+        }
+         */
         //Convert drefs to bytes.
         byte[] drefBytes = dref.getBytes(StandardCharsets.UTF_8);
         if (drefBytes.length == 0) {
@@ -113,44 +189,49 @@ public class ConnectXplane {
             throw new IllegalArgumentException("dref must be less than 255 bytes in UTF-8. Are you sure this is a valid dref?");
         }
 
-        ByteBuffer bb = ByteBuffer.allocate(4 * 1);
-        bb.order(ByteOrder.LITTLE_ENDIAN);
+        //       ByteBuffer bb = ByteBuffer.allocate(4 * 1);
+        //   bb.order(ByteOrder.LITTLE_ENDIAN);
         //for (int j = 0; j < value.length; ++j) {
-        bb.putFloat(value);
+        // bb.putFloat(value);
         //   }
-
         //Build and send message
         //  os.write(value.length);
-        os.write(bb.array());
+        // os.write(bb.array());
         ///   os.write(drefBytes.length);
         os.write(drefBytes, 0, drefBytes.length);
         os.write(0);
-
+        os.writeTo(System.out);
+        /*   int size = os.size();
+        for (int i = size; i < 509; i++) {
+            os.write(0x20);
+        }*/
         int size = os.size();
         for (int i = size; i < 509; i++) {
             os.write(0x20);
         }
 
-        System.err.println(os.size());
         sendUDP(os.toByteArray());
+        try {
+            Thread.sleep(20);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(ConnectXplane.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        /*
+        byte[] data = readUDP();
+        if (data.length == 0) {
+            throw new IOException("No response received.");
+        }
+        if (data.length < 6) {
+            throw new IOException("Response too short");
+        }
+        ByteBuffer rbb = ByteBuffer.wrap(data);
+        rbb.order(ByteOrder.LITTLE_ENDIAN);
+        float aFloat = rbb.getFloat();
+         */
+        return myiD;
+        /**/
     }
-     */
- /*public void sendDREF(String dref, float value) throws IOException {
-        sendDREF(dref, new float[]{value});
-    }
-     */
-    /**
-     * Sends a command to X-Plane that sets the given DREF.
-     *
-     * @param dref The name of the X-Plane dataref to set.
-     * @param value An array of floating point values whose structure depends on
-     * the dref specified.
-     * @throws IOException If the command cannot be sent.
-     */
-    /*  public void sendDREF(String dref, float[] value) throws IOException {
-        sendDREFs(new String[]{dref}, new float[][]{value});
-    }
-     */
+
     void addPAAlt() throws IOException, InterruptedException {
         System.err.println("sending alt to autopilot");
         /*"sim/cockpit/autopilot/altitude", 620.0f
@@ -167,8 +248,12 @@ public class ConnectXplane {
         sendDREF("sim/cockpit/autopilot/autopilot_mode", 2f);
         Thread.sleep(20);
         float value = 1;
-        sendDREF("sim/cockpit/warnings/annunciator_test_pressed", value);
+
+        sendDREF("sim/operation/override/override_annunciators", value);
         Thread.sleep(20);
+
+        /*sendDREF("sim/cockpit/warnings/annunciator_test_pressed", value);
+        Thread.sleep(20);*/
         sendDREF("sim/cockpit/warnings/master_caution_on", value);
         Thread.sleep(20);
         sendDREF("sim/cockpit/warnings/master_warning_on", value);
@@ -181,17 +266,16 @@ public class ConnectXplane {
         Thread.sleep(20);
         sendDREF("sim/cockpit2/annunciators/master_warning", value);
         Thread.sleep(20);
+        /* sendDREF("sim/flightmodel/failures/stallwarning", value);
+        Thread.sleep(20);*/
+
     }
 
     void removePAAlt() throws IOException, InterruptedException {
         System.err.println("sending low to autopilot");
         sendDREF("sim/cockpit/autopilot/autopilot_mode", 0f);
         Thread.sleep(20);
-        /*"sim/cockpit/autopilot/altitude", 620.0f
-         String dref = "sim/cockpit/switches/gear_handle_status";
-    float[] value = {0.0F};
-        
-         */
+
         sendDREF("sim/cockpit/autopilot/altitude", 2000.0f);
         Thread.sleep(20);
         sendDREF("sim/cockpit/radios/com1_freq_hz", 12330.0f);
@@ -202,8 +286,9 @@ public class ConnectXplane {
         sendDREF("sim/cockpit/radios/com1_freq_hz", 12430.0f);
         Thread.sleep(20);
         float value = 0;
-        sendDREF("sim/cockpit/warnings/annunciator_test_pressed", value);
-        Thread.sleep(20);
+
+        /*sendDREF("sim/cockpit/warnings/annunciator_test_pressed", value);
+        Thread.sleep(20);*/
         sendDREF("sim/cockpit/warnings/master_caution_on", value);
         Thread.sleep(20);
         sendDREF("sim/cockpit/warnings/master_warning_on", value);
@@ -212,8 +297,35 @@ public class ConnectXplane {
         Thread.sleep(20);
         sendDREF("sim/cockpit/warnings/annunciators/master_warning", value);
         Thread.sleep(20);
+        sendDREF("sim/cockpit2/annunciators/master_caution", value);
+        Thread.sleep(20);
 
-        //sendDREF("sim/cockpit2/annunciators/master_caution", value);
-        //   sendDREF("sim/cockpit2/annunciators/master_warning", value);
+        sendDREF("sim/operation/override/override_annunciators", value);
+        Thread.sleep(20);
+        /*   sendDREF("sim/flightmodel/failures/stallwarning", value);
+        Thread.sleep(20);*/
+    }
+
+    void masterCaution(boolean cc) throws IOException {
+
+        float value = (cc) ? 1.0f : 0.0f;
+        sendDREF("sim/operation/override/override_annunciators", 1.0f);
+        sendDREF("sim/cockpit/warnings/master_caution_on", value);
+        sendDREF("sim/cockpit/warnings/annunciators/master_caution", value);
+
+    }
+
+    void masterWarning(boolean cc) throws IOException {
+
+        float value = (cc) ? 1.0f : 0.0f;
+        sendDREF("sim/operation/override/override_annunciators", 1.0f);
+        sendDREF("sim/cockpit/warnings/master_warning_on", value);
+        sendDREF("sim/cockpit/warnings/annunciators/master_warning", value);
+
+    }
+
+    float getAPAlt() throws IOException {
+        int id = getDREFs("sim/cockpit/autopilot/altitude", 3);
+        return 0f;
     }
 }
--- xplanecontroler/src/main/java/fr/irit/ics/circus/hamsters/xplanecontroler/Main.java
+++ xplanecontroler/src/main/java/fr/irit/ics/circus/hamsters/xplanecontroler/Main.java
@@ -5,6 +5,8 @@
  */
 package fr.irit.ics.circus.hamsters.xplanecontroler;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.SocketException;
 import java.net.UnknownHostException;
@@ -22,9 +24,21 @@ public class Main extends javax.swing.JFrame {
     /**
      * Creates new form Main
      */
-    public Main() throws SocketException, UnknownHostException {
+    public Main() throws SocketException, UnknownHostException, IOException {
         connectXplane = new ConnectXplane();
         initComponents();
+        System.err.println("alt" + connectXplane.getAPAlt());
+      /*  new javax.swing.Timer(1500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+
+                } catch (IOException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        ).start();*/
     }
 
     /**
@@ -44,6 +58,8 @@ public class Main extends javax.swing.JFrame {
         jTextField3 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jToggleWarning = new javax.swing.JToggleButton();
+        jToggleCaution = new javax.swing.JToggleButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -73,30 +89,51 @@ public class Main extends javax.swing.JFrame {
             }
         });
 
+        jToggleWarning.setText("Master Warning");
+        jToggleWarning.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleWarningActionPerformed(evt);
+            }
+        });
+
+        jToggleCaution.setText("Master Caution");
+        jToggleCaution.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jToggleCautionActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
-                .addGap(18, 18, 18)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 382, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel3)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jLabel1)
+                        .addGap(18, 18, 18)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jButton1)
+                            .addComponent(jButton2))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 382, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(62, 62, 62))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(62, 62, 62))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jToggleWarning)
+                            .addComponent(jToggleCaution))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -119,7 +156,11 @@ public class Main extends javax.swing.JFrame {
                         .addComponent(jLabel3)
                         .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(jButton2))
-                .addContainerGap(410, Short.MAX_VALUE))
+                .addGap(82, 82, 82)
+                .addComponent(jToggleWarning)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jToggleCaution)
+                .addContainerGap(276, Short.MAX_VALUE))
         );
 
         pack();
@@ -136,7 +177,7 @@ public class Main extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-       try {
+        try {
             connectXplane.removePAAlt();
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
@@ -144,6 +185,28 @@ public class Main extends javax.swing.JFrame {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jToggleCautionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleCautionActionPerformed
+        try {
+            /* if (jToggleButton2.isSelected()) {
+            
+            } else {
+            
+            }*/
+            connectXplane.masterCaution(jToggleCaution.isSelected());
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jToggleCautionActionPerformed
+
+    private void jToggleWarningActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleWarningActionPerformed
+        try {
+            // TODO add your handling code here:
+            connectXplane.masterWarning(jToggleWarning.isSelected());
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jToggleWarningActionPerformed
 
     /**
      * @param args the command line arguments
@@ -177,9 +240,9 @@ public class Main extends javax.swing.JFrame {
             public void run() {
                 try {
                     new Main().setVisible(true);
-                } catch (SocketException ex) {
+                } catch (SocketException | UnknownHostException ex) {
                     Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (UnknownHostException ex) {
+                } catch (IOException ex) {
                     Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
@@ -195,5 +258,7 @@ public class Main extends javax.swing.JFrame {
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
+    private javax.swing.JToggleButton jToggleCaution;
+    private javax.swing.JToggleButton jToggleWarning;
     // End of variables declaration//GEN-END:variables
 }
